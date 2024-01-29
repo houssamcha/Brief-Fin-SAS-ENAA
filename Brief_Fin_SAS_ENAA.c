66d36e4171de73f4d94d517a3e31b2ec619d920c
houssamcha <chagourihoussam@gmail.com>
2024-01-29 12:03:44 +0100

    BRIEF SAS

--- Brief_Fin_SAS_ENAA.c
+++ Brief_Fin_SAS_ENAA.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+int index;
 int taille = 0;
 struct Tache{
         char name[50];
@@ -9,47 +10,60 @@ void ajouter(struct Tache task[]) {
     scanf("%s", task[taille].name);    
     printf("Entrer la description :\n");
     scanf("%s", task[taille].description);
+    //while (getchar() != '\n');
     taille++;
 }
-
 void afficher(struct Tache task[]){
     for (int i = 0; i < taille; i++)
     {
-        printf("tache %d: %s",task[i].name);
-        printf("tache : %s",task[i].description);
+        printf("tache %d: %s \n",i + 1,task[i].name);
+        printf("Sa description est : %s \n",task[i].description);
+        printf("\n");
     }
 }
-int menu(){
+void modifier(struct Tache task[]){
+    printf("Quelle tache souhaitez-vous modifier ?\n");
+    scanf("%d",&index);
+    if(index >= 1 && index <= taille){
+        printf("Entrer le nouveau nom : \n");
+        scanf("%s",task[index - 1].name);
+        printf("Entrer la nouvelle description : \n");
+        scanf("%s",task[index - 1].description);
+    }
+    else
+    printf("choix invalid !");
+}
+void menu(struct Tache task[]){
     int choix;
+    do{
     printf("MENU\n");
     printf("1 AJOUTER\n");
     printf("2 AFFICHER\n");
-    printf("3 SUPPRIMER\n");
-    printf("4 MODIFIER\n");
-    printf("5 ORDONNER\n");
-    printf("6 FILTRER\n");
-    printf("7 QUITTER\n");
+    printf("3 MODIFIER\n");
+    printf("4 SUPPRIMER\n");
+    printf("5 QUITTER\n");
     scanf("%d",&choix);
-    return choix;
-}
-int main (){
-    struct Tache task[50];
-    do{
-        switch (menu())
+        switch (choix)
         {
         case 1:
             ajouter(task);
-            taille++;
             break;
         case 2:
-        afficher(task);
-        taille++;
-        break;
-        default:
-        printf("Choix invalide. Entrer un choix entre 1 et 7!\n");
+            afficher(task);
             break;
+        case 3:
+            modifier(task);
+            break;
+        case 5:
+            printf("Au revoir !");
+            break; 
+        default:
+            printf("Choix invalide. Entrer un choix entre 1 et 7!\n");
         }
-    }while(menu() != 3);
-
+    }while(choix != 5);
+}
+int main (){
+    struct Tache task[50];
+    menu(task);
     return 0;
 }
